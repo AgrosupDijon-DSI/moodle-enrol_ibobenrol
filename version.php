967e8d53de7e0e3d96629203e603e328d3ee4a8a
GREBOT Frédéric <frederic.grebot@agrosupdijon.fr>
2024-04-30 14:09:11 +0200

    Update lang strings

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_ibobenrol'; // Full name of the plugin (used for diagnostics).
-$plugin->release   = '2.0 (2023051829)';
-$plugin->version   = 2023051829;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '2.0 (2023051830)';
+$plugin->version   = 2023051830;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019052002;         // Requires this Moodle version (3.9).
 $plugin->maturity  = MATURITY_STABLE;
